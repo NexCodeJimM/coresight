1bc549df51a320173444029819002cc56411d381
NexCodeJimM <jimmendozadev@gmail.com>
2024-11-12 07:21:46 +0800

    changed some settings in the backend.

--- backend/config.js
+++ backend/config.js
@@ -1,20 +1,15 @@
-const config = {
-  development: {
-    port: 3000,
-    influxdb: {
-      url: "http://localhost:8086",
-      org: "EFI",
-      bucket: "efi_servers",
-    },
+module.exports = {
+  influxdb: {
+    host: "efi",
+    database: "efi_servers",
+    username: "jimm", // if required
+    password: "Rjmendoza21!", // if required
   },
-  production: {
-    port: process.env.PORT || 3000,
-    influxdb: {
-      url: process.env.INFLUXDB_URL,
-      org: process.env.INFLUXDB_ORG,
-      bucket: process.env.INFLUXDB_BUCKET,
+  server: {
+    port: 3000,
+    cors: {
+      origin: ["http://localhost:3000", "http://165.22.237.60:3000"],
+      credentials: true,
     },
   },
 };
-
-module.exports = config[process.env.NODE_ENV || "development"];
