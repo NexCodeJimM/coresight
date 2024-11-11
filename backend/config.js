cd0bab4b0334f326200b138c1408e800a06da6d2
NexCodeJimM <jimmendozadev@gmail.com>
2024-11-12 07:44:24 +0800

    changed the host name.

--- backend/config.js
+++ backend/config.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-const config = {
-  development: {
-    port: 3000,
-    influxdb: {
-      url: "http://165.22.237.60:8086",
-      org: "EFI",
-      bucket: "efi_servers",
-    },
-=======
 module.exports = {
   influxdb: {
-    host: "efi",
+    host: "intel-test-server-ca",
     database: "efi_servers",
     username: "jimm", // if required
     password: "Rjmendoza21!", // if required
->>>>>>> 1bc549df51a320173444029819002cc56411d381
   },
   server: {
     port: 3000,
